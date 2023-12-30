998aa813bd05deeb27d66788cf88c38efbe9e6fd
ThinhMaiQuoc <thinh.quocc@gmail.com>
2023-12-30 18:20:02 +0700

    email validation, return job id for result query

--- src/queues/scarpingQueue.js
+++ src/queues/scarpingQueue.js
@@ -5,10 +5,13 @@ import searchResultService from '../services/searchResult.service.js';
 const scrapingQueue = new Queue('scraping', 'redis://127.0.0.1:6379');
 
 scrapingQueue.process(async (job) => {
-    const { keyword_id, keyword } = job.data;
+    const { keyword_id, keyword, jobId } = job.data;
     const results = await scrapeSearchResults(keyword);
-    await searchResultService.addSearchResult(keyword_id, results);
-    return results;
+
+    await searchResultService.addSearchResult(keyword_id, {
+        ...results,
+        jobId: jobId
+    });
 });
 
 export default scrapingQueue;
--- src/services/searchResult.service.js
+++ src/services/searchResult.service.js
@@ -1,11 +1,13 @@
 import db from '../configs/database.config.js';
+import { DatabaseError } from '../errors/database.error.js';
 
 class SearchResultService {
     async addSearchResult(keywordId, results) {
         try {
-            const { adWordsCount, links, searchInfo, htmlContent } = results;
+            const { adWordsCount, links, searchInfo, htmlContent, jobId } = results;
             await db('search_results').insert({
                 keyword_id: keywordId,
+                job_id: jobId,
                 adwords_count: adWordsCount,
                 link_count: links.length,
                 total_results: searchInfo,
@@ -13,6 +15,31 @@ class SearchResultService {
             });
         } catch (error) {
             throw new DatabaseError(`Failed add search result: ${error.message}`);
+        }
+    }
+
+    async getResultsForExistingKeywords(keywordIds) {
+        try {
+            const results = await db('search_results')
+                .whereIn('keyword_id', keywordIds)
+                .select('keyword_id', 'adwords_count', 'link_count', 'total_results', 'html_code');
+
+            return results;
+        } catch (error) {
+            throw new DatabaseError(`Failed to retrieve search results: ${error.message}`);
+        }
+    }
+
+    async getResultsForKeywords(jobIds) {
+        try {
+            const results = await db('search_results')
+                .join('keywords', 'search_results.keyword_id', '=', 'keywords.keyword_id')
+                .whereIn('search_results.job_id', jobIds)
+                .select('search_results.*', 'keywords.keyword');
+
+            return results;
+        } catch (error) {
+            throw new DatabaseError(`Failed to retrieve search results: ${error.message}`);
         }
     }
 }
